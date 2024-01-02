9d21d653f74bd4ba67dcb13b20aa6c3dc9b19ce6
Daniel Ireson <direson@opentable.com>
2024-01-02 19:26:37 +0000

    style error page

--- app.js
+++ app.js
@@ -48,9 +48,10 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.status = res.locals.error.status ?? 500;
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(res.locals.status);
   res.render("error");
 });
 
