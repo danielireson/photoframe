e25d773f964ee136a3fdd87b0435717a4c9897be
Daniel Ireson <danielireson@gmail.com>
2024-04-03 23:29:15 +0100

    preload images

--- public/js/app.js
+++ public/js/app.js
@@ -2,33 +2,71 @@ const initialData = window.__INITIAL_DATA__ ?? {};
 const images = initialData.images ?? [];
 const app = document.getElementById("app");
 
-if (!images.length) {
+let currentImageIndex = -1;
+
+const showMessage = (message) => {
+  // toggle message view
   app.classList.add("is-message");
 
+  // create message element
   const h1 = document.createElement("h1");
-  const text = document.createTextNode("No images to display");
+  const text = document.createTextNode(message);
 
+  // append message to app
   h1.appendChild(text);
   app.appendChild(h1);
-} else {
+};
+
+const incrementImageIndex = () => {
+  if (currentImageIndex < images.length - 1) {
+    // next image exists
+    currentImageIndex++;
+  } else {
+    // end of images
+    currentImageIndex = 0;
+  }
+};
+
+const setCurrentImage = (slide) => {
+  slide.style.backgroundImage = `url(${images[currentImageIndex]})`;
+
+  // preload next image to avoid flicker
+  if (currentImageIndex < images.length - 1) {
+    new Image().src = images[currentImageIndex + 1];
+  }
+};
+
+const nextSlide = (slide) => {
+  incrementImageIndex();
+  setCurrentImage(slide);
+};
+
+const showSlideshow = () => {
+  // toggle slideshow view
   app.classList.add("is-slideshow");
 
+  // create slide element
   const slide = document.createElement("div");
   slide.classList.add("slide");
-  slide.style.backgroundImage = `url(${images[0]})`;
 
+  // append slide to app
   app.appendChild(slide);
 
-  const maxImageIndex = images.length - 1;
-  let currentImageIndex = 0;
+  // initialize first slide
+  nextSlide(slide);
 
+  // schedule future slides
   setInterval(() => {
-    if (currentImageIndex < maxImageIndex) {
-      currentImageIndex++;
-    } else {
-      currentImageIndex = 0;
-    }
-
-    slide.style.backgroundImage = `url(${images[currentImageIndex]})`;
+    nextSlide(slide);
   }, 2000);
-}
+};
+
+const initializeApp = () => {
+  if (!images.length) {
+    showMessage("No images to display");
+  } else {
+    showSlideshow();
+  }
+};
+
+initializeApp();
