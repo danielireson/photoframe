7f57166ba8635a48f4103dbbdab2debbaaac4f09
Daniel Ireson <danielireson@gmail.com>
2024-04-03 22:29:43 +0100

    add slideshow to page

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,34 @@
 const initialData = window.__INITIAL_DATA__ ?? {};
 const images = initialData.images ?? [];
-const appContainer = document.getElementById("app");
+const app = document.getElementById("app");
+
+if (!images.length) {
+  app.classList.add("is-message");
+
+  const h1 = document.createElement("h1");
+  const text = document.createTextNode("No images to display");
+
+  h1.appendChild(text);
+  app.appendChild(h1);
+} else {
+  app.classList.add("is-slideshow");
+
+  const slide = document.createElement("div");
+  slide.classList.add("slide");
+  slide.style.backgroundImage = `url(${images[0]})`;
+
+  app.appendChild(slide);
+
+  const maxImageIndex = images.length - 1;
+  let currentImageIndex = 0;
+
+  setInterval(() => {
+    if (currentImageIndex < maxImageIndex) {
+      currentImageIndex++;
+    } else {
+      currentImageIndex = 0;
+    }
+
+    slide.style.backgroundImage = `url(${images[currentImageIndex]})`;
+  }, 2000);
+}
